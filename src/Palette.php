62e95d64d8975ff166175adbd54655db8bb7d0fe
Ari <aristath@gmail.com>
2019-02-24 12:08:06 +0200

    remove kirki-script dependency

--- src/Palette.php
+++ src/Palette.php
@@ -50,7 +50,6 @@ class Palette extends Base {
 			'kirki-control-palette',
 			"$url/assets/scripts/control.js",
 			[
-				'kirki-script',
 				'jquery',
 				'customize-base',
 				'kirki-dynamic-control',
